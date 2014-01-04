535f9c04e4650c98a8abca31ab78c86404aca793
bertt_cp <SND\bertt_cp>
2014-01-04 01:19:07 -0800

    Adding unit test project

--- trunk/ArcBruTile/app/lib/FileCacheExtensionMethods.cs
+++ trunk/ArcBruTile/app/lib/FileCacheExtensionMethods.cs
@@ -8,6 +8,29 @@ namespace BrutileArcGIS.lib
 {
     public static class ExtensionMethods
     {
+        public static void AddWorldFile(this FileCache fileCache, TileInfo tileInfo, int width, int height, string format)
+        {
+            var fileName = fileCache.GetFileName(tileInfo.Index);
+            var fi = new FileInfo(fileName);
+
+            var tfwFile = fileName.Replace(fi.Extension, "." + GetWorldFile(format));
+
+            var extent = tileInfo.Extent;
+
+            using (var sw = new StreamWriter(tfwFile))
+            {
+                var resX = (extent.MaxX - extent.MinX) / width;
+                var resY = (extent.MaxY - extent.MinY) / height;
+                sw.WriteLine(resX.ToString(CultureInfo.InvariantCulture));
+                sw.WriteLine("0");
+                sw.WriteLine("0");
+                sw.WriteLine((resY * -1).ToString(CultureInfo.InvariantCulture));
+                sw.WriteLine(extent.MinX.ToString(CultureInfo.InvariantCulture));
+                sw.WriteLine(extent.MaxY.ToString(CultureInfo.InvariantCulture));
+                sw.Close();
+            }
+        }
+
         private static string GetWorldFile(string format)
         {
             var res = String.Empty;
@@ -40,27 +63,5 @@ namespace BrutileArcGIS.lib
 
         }
 
-        public static void AddWorldFile(this FileCache fileCache, TileInfo tileInfo, int width, int height, string format)
-        {
-            var fileName = fileCache.GetFileName(tileInfo.Index);
-            var fi = new FileInfo(fileName);
-
-            var tfwFile = fileName.Replace(fi.Extension, "." + GetWorldFile(format));
-
-            var extent = tileInfo.Extent;
-
-            using (var sw = new StreamWriter(tfwFile))
-            {
-                var resX = (extent.MaxX - extent.MinX) / width;
-                var resY = (extent.MaxY - extent.MinY) / height;
-                sw.WriteLine(resX.ToString(CultureInfo.InvariantCulture));
-                sw.WriteLine("0");
-                sw.WriteLine("0");
-                sw.WriteLine((resY * -1).ToString(CultureInfo.InvariantCulture));
-                sw.WriteLine(extent.MinX.ToString(CultureInfo.InvariantCulture));
-                sw.WriteLine(extent.MaxY.ToString(CultureInfo.InvariantCulture));
-                sw.Close();
-            }
-        }
     }
 }
