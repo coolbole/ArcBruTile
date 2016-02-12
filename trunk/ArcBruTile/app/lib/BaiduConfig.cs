f2565a3344c82f90617127f87a829b650fc60041
bertt <btemme@hotmail.com>
2016-02-13 00:09:47 +0100

    forgot these files

---
+++ trunk/ArcBruTile/app/lib/BaiduConfig.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using BrutileArcGIS.Lib;
+using BruTile;
+using BruTile.Web;
+
+namespace BrutileArcGIS.lib
+{
+    public class BaiduConfig : IConfig
+    {
+        public BaiduConfig(string name, string url)
+        {
+            Name = name;
+            Url = url;
+        }
+
+        public ITileSource CreateTileSource()
+        {
+            
+            var tileSchema = new BaiduTileSchema();
+            var servers = new List<string> { "1", "2", "3", "4", "5","6","7","8","9" };
+            var baiduRequest = new BasicRequest(Url, servers);
+            var tileProvider = new WebTileProvider(baiduRequest);
+            var tileSource = new TileSource(tileProvider, tileSchema);
+            return tileSource;
+        }
+        public string Name { get; set; }
+        public string Url { get; set; }
+    }
+}
---
+++ trunk/ArcBruTile/app/lib/BaiduTileSchema.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using BruTile;
+
+namespace BrutileArcGIS.lib
+{
+    public class BaiduTileSchema : TileSchema
+    {
+        public BaiduTileSchema()
+        {
+            double[] resolutions = new double[19];
+            for (var i = 0; i < 19; i++)
+            {
+                resolutions[i] = Math.Pow(2, 18 - i);
+                var res = new Resolution
+                {
+                    Id = i.ToString(),
+                    UnitsPerPixel = resolutions[i]
+                };
+                Resolutions[res.Id] = new Resolution { Id = res.Id, UnitsPerPixel = res.UnitsPerPixel };
+            }
+            Height = 256;
+            Width = 256;
+            Extent = new Extent(-20037508.34, -20037508.34, 20037508.34, 20037508.34);
+            OriginX = 0;
+            OriginY = 0;
+            Format = "png";
+            Name = "BAIDU";
+            Axis = AxisDirection.Normal;
+            Srs = "EPSG:900913";
+        }
+    }
+}
