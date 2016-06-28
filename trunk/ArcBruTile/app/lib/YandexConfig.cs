1a2105be67c04d072bb1311f3172afc919a82e1f
bertt <btemme@hotmail.com>
2016-06-28 16:25:11 +0200

    added Yandex config

---
+++ trunk/ArcBruTile/app/lib/YandexConfig.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using BrutileArcGIS.Lib;
+using BruTile;
+using BruTile.Predefined;
+using BruTile.Web;
+
+namespace BrutileArcGIS.lib
+{
+    public class YandexConfig : IConfig
+    {
+        private string _name;
+        private string _url;
+
+        public YandexConfig(string name, string url)
+        {
+            _name = name;
+            _url = url;
+        }
+
+        public ITileSource CreateTileSource()
+        {
+            var tileSchema = new GlobalSphericalMercator();
+            var servers = new List<string> { "01", "02", "03", "04" };
+            var nokiaRequest = new BasicRequest(_url, servers);
+            var tileProvider = new WebTileProvider(nokiaRequest);
+            var tileSource = new TileSource(tileProvider, tileSchema);
+            return tileSource;
+        }
+
+        public string Url
+        {
+            get { return _url; }
+        }
+    }
+}
